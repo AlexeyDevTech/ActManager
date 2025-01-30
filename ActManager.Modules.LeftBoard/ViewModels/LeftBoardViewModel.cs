6e086c95d3315dabb0417bf01ac53d9e8a052cfb
alexey <strannik9717@hotmail.com>
2025-01-30 16:24:10 +0300

    mod

--- ActManager.Modules.LeftBoard/ViewModels/LeftBoardViewModel.cs
+++ ActManager.Modules.LeftBoard/ViewModels/LeftBoardViewModel.cs
@@ -1,16 +1,30 @@
 ﻿using ActManager.Domain.Models;
 using ActManager.Domain.Repositories;
+using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Regions;
+using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Input;
 
 namespace ActManager.Modules.LeftBoard.ViewModels
 {
     public class LeftBoardViewModel : BindableBase
     {
+        //basic
+        IRegionManager regionManager;
+
         public ObservableCollection<Building> Constructions { get; set; }
 
-        public LeftBoardViewModel()
+        public ICommand SelectCommand { get; set; }
+        public LeftBoardViewModel(IRegionManager regionManager)
         {
+            this.regionManager = regionManager;
+            //commands
+            SelectCommand = new DelegateCommand<object>(SelectBuilding);
+
             Constructions = new ObservableCollection<Building>();
             var rep = new BuildingsRepository();
             var list = rep.GetAll();
@@ -18,6 +32,13 @@ namespace ActManager.Modules.LeftBoard.ViewModels
             {
                 Constructions.Add(item);
             }
+        }
+
+        private void SelectBuilding(object obj)
+        {
+            int inst = (int)obj;
+            var bld = Constructions.FirstOrDefault(x => x.ID == inst);
+            Debug.WriteLine(bld.Name);
         }
     }
 }
