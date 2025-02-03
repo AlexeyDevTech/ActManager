0947aff1b20a51502036c5596964cceeaacb5efb
alexey <strannik9717@hotmail.com>
2025-02-03 11:38:13 +0300

    styling header

--- ActManager/Views/MainWindow.xaml.cs
+++ ActManager/Views/MainWindow.xaml.cs
@@ -7,9 +7,19 @@ namespace ActManager.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool C0IsCollapsed = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!C0IsCollapsed)
+                C0.Width = new GridLength(280,GridUnitType.Pixel);
+            else
+                C0.Width = new GridLength(8, GridUnitType.Star);
+            C0IsCollapsed = !C0IsCollapsed;
         }
     }
 }
