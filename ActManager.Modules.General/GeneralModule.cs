59a6a1f98414bd5acefea95494a4b162edaa21fb
alexey <strannik9717@hotmail.com>
2025-02-06 15:32:26 +0300

    add first component

--- ActManager.Modules.General/GeneralModule.cs
+++ ActManager.Modules.General/GeneralModule.cs
@@ -1,4 +1,5 @@
 ﻿using ActManager.Core;
+using ActManager.Modules.General.Components.Views;
 using ActManager.Modules.General.ViewModels;
 using ActManager.Modules.General.Views;
 using Prism.Ioc;
@@ -21,7 +22,7 @@ namespace ActManager.Modules.General
             
             _regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, containerProvider.Resolve<GeneralView>);
             _regionManager.RegisterViewWithRegion(RegionNames.GeneralContentRegion, containerProvider.Resolve<MainMenuView>);
-            
+            _regionManager.RegisterViewWithRegion("MainMenuItem1", containerProvider.Resolve<TodayComponentView>);
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
